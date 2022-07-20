596572ac57b9b6debe92efbc86f30cbaab777cf5
Brandon <brandon.r.young@outlook.com>
2022-07-20 14:07:13 -0400

    Start modal and JS functionality

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const addBtn = document.querySelector('.addBook');
+const modal = document.querySelector(".modal");
+const span = document.querySelector(".close")
+
 let library = [];
 
 function Book(title, author, pages, read) {
@@ -12,4 +16,16 @@ function addBook(title, author, pages, read) {
     library.push(newBook);
 }
 
-console.log(library);
+addBtn.onclick = function() {
+    modal.style.display = "block"
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+  
+window.onclick = function(event) {
+    if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
